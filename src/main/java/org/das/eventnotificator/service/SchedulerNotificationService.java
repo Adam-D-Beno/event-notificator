da3f200aab93c0eaa44f34c9b6cea22e5dc2447f
ПК_Home <wrofdt@ya.ru>
2025-05-18 18:43:11 +0300

    refactor SchedulerNotificationService

--- src/main/java/org/das/eventnotificator/service/SchedulerNotificationService.java
+++ src/main/java/org/das/eventnotificator/service/SchedulerNotificationService.java
@@ -1,17 +1,12 @@
 package org.das.eventnotificator.service;
 
 import lombok.RequiredArgsConstructor;
-import org.das.eventnotificator.model.entity.NotificationEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.time.Period;
 import java.util.List;
 
 @Service
